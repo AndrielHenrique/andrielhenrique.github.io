d29ce73aa97d9ef5c9dc233658c1ef92d38dbed0
Andriel <134160567+AndrielHenrique@users.noreply.github.com>
2023-06-26 00:13:26 -0300

    edit1.3.3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,6 +19,7 @@ function redirecionarp(event) {
             window.location.href = url;
         }
 }
+//script para o audio
 function playAudio(audioId) {
     var audio = document.getElementById(audioId);
     if (audio.paused) {
@@ -28,3 +29,26 @@ function playAudio(audioId) {
       audio.currentTime = 0;
     }
   }
+
+  //DOM CARRINHO
+  window.addEventListener("DOMContentLoaded", function() {
+    var carrinhos = document.querySelectorAll(".carrinho");
+    carrinhos.forEach(function(carrinho) {
+      carrinho.addEventListener("click", function(event) {
+        alert("O produto foi adicionado ao carrinho!");
+      });
+    });
+  });
+//DOM PROMOÇAO RELAMPAGO
+  window.addEventListener("DOMContentLoaded", function() {
+    var toggleButton = document.getElementById("toggleButton");
+    var elementoOculto = document.getElementById("elementoOculto");
+  
+    toggleButton.addEventListener("click", function() {
+      if (elementoOculto.style.display === "none") {
+        elementoOculto.style.display = "block";
+      } else {
+        elementoOculto.style.display = "none";
+      }
+    });
+  });
